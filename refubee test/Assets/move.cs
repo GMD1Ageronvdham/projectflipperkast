b607a561d73c933767825ed774df74b970a18d46
Geron van den Ham <geron.ham@xs4all.nl>
2017-03-05 20:24:19 +0100

    refubee test

---
+++ refubee test/Assets/move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+    public float speed;
+    public float jumpspeed;
+    public float flyjumps;
+    public float maxjumps;
+	void Start () {
+		
+	}
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * speed;
+
+        transform.Translate(horizontal, 0, 0);
+        if (Input.GetButtonDown("Jump") && flyjumps < maxjumps)
+        {
+            this.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpspeed);
+            flyjumps += 1;
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        flyjumps = 0;
+    }
+}
